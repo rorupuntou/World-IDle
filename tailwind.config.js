d9cd2817dfb3a847bf842827244fc7929fc1f498
rorupuntou <rorupuntou@gmail.com>
2025-10-14 19:28:20 -0300

    feat: add custom font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@worldcoin/mini-apps-ui-kit-react/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {

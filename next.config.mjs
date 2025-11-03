7054c83f6b63f30bd3914d921ef61060982fa07d
rorupuntou <rorupuntou@gmail.com>
2025-11-02 21:49:39 -0300

    fix: Remove workflow config from next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import { withWorkflow } from 'workflow/next';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
 
-export default withWorkflow(nextConfig);
+export default nextConfig;
